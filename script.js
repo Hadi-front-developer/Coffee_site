aa961202393a36388781cd19ba25b9b2aaf62fb0
Hadi <jafarihadi661@gamil.com>
2025-04-16 12:07:33 -0700

    coffee-site-finish

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ function showSlider(type){
      let thumbnailSlider = document.querySelectorAll('.carousel .thumbnail .item')
      if(type === 'next'){
           list.appendChild(itemSlider[0])
-          console.log(thumbnailSlider)
           thumbnail.appendChild(thumbnailSlider[0])
           carsoul.classList.add('next')
           
@@ -40,7 +39,6 @@ function showSlider(type){
 runTimeOut = setTimeout(() => {
      carsoul.classList.remove('next')
      carsoul.classList.remove('prev');
-     console.log('hadi')
 }, timeRunning);
 
 clearTimeout(runAuto);
@@ -49,3 +47,46 @@ runAuto = setTimeout(() => {
 }, timeAutoRun);
 }
 
+// search-bar
+
+const input_box = document.querySelector('.input_box')
+const search = document.querySelector('.search')
+const close_icon = document.querySelector('.close_icon')
+
+console.log(input_box, search, close_icon)
+
+search.addEventListener('click', () =>  input_box.classList.add('open'))
+close_icon.addEventListener('click', () =>  input_box.classList.remove('open'))
+
+
+
+// SWIPER
+
+new Swiper('.card-wrapper', {
+
+     loop: true,
+     slidesPerView: 3,
+     spaceBetween: 30,
+
+
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+       dynamicBullets: true,
+     },
+   
+     breakpoints: {
+          0: {
+            slidesPerView: 1
+
+          },
+          768: {
+            slidesPerView: 2
+
+          },
+          1024: {
+            slidesPerView: 3
+          },
+        },
+
+   });
